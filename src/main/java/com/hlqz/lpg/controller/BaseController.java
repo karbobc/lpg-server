eef4d48d5d73dbed0bba722332f39d0ef46862c5
Karbob <karbobc@gmail.com>
2024-02-01 00:09:53 +0800

    feat: 简单修饰BaseController

--- src/main/java/com/hlqz/lpg/controller/BaseController.java
+++ src/main/java/com/hlqz/lpg/controller/BaseController.java
@@ -1,8 +1,17 @@
 package com.hlqz.lpg.controller;
 
+import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 /**
  * @author Karbob
  * @date 2023-11-20
  */
 public class BaseController {
+
+    @Resource
+    private HttpServletRequest request;
+    @Resource
+    private HttpServletResponse response;
 }
