e9df756eb791885ae1d9e2e258e207d89b7933f1
Karbob <karbobc@gmail.com>
2024-10-09 17:24:00 +0800

    feat: add JVM_OPTS env support

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,12 @@ JVM_OPTS="-Xmx512m \
           -XX:+UseZGC \
           -XX:+ZGenerational \
           -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector \
-          -javaagent:/app/$(sed -n 's|.*"\(.*transmittable-thread-local.*\)".*|\1|p' BOOT-INF/classpath.idx)"
+          -javaagent:/app/$(sed -n 's|.*"\(.*transmittable-thread-local.*\)".*|\1|p' BOOT-INF/classpath.idx) \
+          ${JVM_OPTS}"
 
 # shellcheck disable=SC2068,SC2086
-exec java ${JVM_OPTS} org.springframework.boot.loader.launch.JarLauncher ${@}
+if [ $# -eq 0 ]; then
+  exec java ${JVM_OPTS} org.springframework.boot.loader.launch.JarLauncher ${@}
+else
+  exec ${@}
+fi
