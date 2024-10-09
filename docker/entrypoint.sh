79084f317411ddde8d1c874d383a9f190c963c1a
Karbob <karbobc@gmail.com>
2024-10-09 22:23:54 +0800

    feat: layered packaging (#52)
    * feat: layered packaging
    
    * feat: add `JVM_OPTS` env support

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env sh
 
-exec java ${JVM_OPTS} -jar /app/lpg-server.jar
+JVM_OPTS="-Xmx512m \
+          -Xms512m \
+          -XX:+UseZGC \
+          -XX:+ZGenerational \
+          -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector \
+          -javaagent:/app/$(sed -n 's|.*"\(.*transmittable-thread-local.*\)".*|\1|p' BOOT-INF/classpath.idx) \
+          ${JVM_OPTS}"
+
+# shellcheck disable=SC2068,SC2086
+if [ $# -eq 0 ]; then
+  exec java ${JVM_OPTS} org.springframework.boot.loader.launch.JarLauncher ${@}
+else
+  exec ${@}
+fi
